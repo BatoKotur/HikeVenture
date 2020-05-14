325a8f85891c4fdedddb40502095027f3fab860b
Bato Kotur <bato97@outlook.de>
2020-05-14 10:00:53 +0200

    Add Material to Mesh Cube

--- FirstTest/Main.js
+++ FirstTest/Main.js
@@ -1,17 +1,21 @@
 "use strict";
 var FirstTest;
 (function (FirstTest) {
-    console.log("Hello");
     window.addEventListener("load", init);
     var f = FudgeCore;
     function init(_event) {
         let node = new f.Node("Node");
-        let cmpCamera = new f.ComponentCamera();
-        let canvas = document.querySelector("canvas");
+        const canvas = document.querySelector("canvas");
         console.log(canvas);
+        let zPos = 4;
+        let cmpCamera = new f.ComponentCamera();
+        cmpCamera.pivot.translateZ(zPos);
         let mesh = new f.MeshCube();
         let cmpMesh = new f.ComponentMesh(mesh);
         node.addComponent(cmpMesh);
+        let mtrSolidWhite = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(new f.Color(Math.random(), Math.random(), Math.random(), 1)));
+        let cmpMaterial = new f.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
         let viewport = new f.Viewport();
         viewport.initialize("Viewport", node, cmpCamera, canvas);
         viewport.draw();
--- FirstTest/Main.ts
+++ FirstTest/Main.ts
@@ -1,18 +1,24 @@
 namespace FirstTest {
-    console.log("Hello");
 
     window.addEventListener("load", init);
     import f = FudgeCore;
 
     function init(_event: Event): void {
         let node: f.Node = new f.Node("Node");
-        let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
-        let canvas: HTMLCanvasElement = document.querySelector("canvas");
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
         console.log(canvas);
+        
+        let zPos: number = 4;
+        let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
+        cmpCamera.pivot.translateZ(zPos);
 
         let mesh: f.MeshCube = new f.MeshCube();
         let cmpMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
         node.addComponent(cmpMesh);
+
+        let mtrSolidWhite: f.Material = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(new f.Color(Math.random(), Math.random(), Math.random(), 1)));
+        let cmpMaterial: f.ComponentMaterial = new f.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
         
         let viewport: f.Viewport = new f.Viewport();
         viewport.initialize("Viewport", node, cmpCamera, canvas);
